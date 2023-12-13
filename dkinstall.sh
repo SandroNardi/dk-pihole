d8c30fd3ec446abc6bcc840bb604fe06a20d6ded
SandroN <51317651+SandroNardi@users.noreply.github.com>
2023-12-13 12:05:49 +0100

    Update dkinstall.sh

--- dkinstall.sh
+++ dkinstall.sh
@@ -1,6 +1,10 @@
+echo "Stopping container..."
 docker stop pihole
+echo "Removing old images and container..."
 docker image rm pihole
 docker container rm pihole
+echo "Pulling new image..."
 docker pull pihole/pihole
+echo "Composing..."
 docker compose up -d
 
